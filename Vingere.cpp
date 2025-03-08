728854a68ae6d0522838b575ff80d24bc562b921
smaranvoora <smaranvoora@gmail.com>
2025-03-08 00:40:55 -0600

    March 8th 12:40 AM Initial commit

---
+++ Vingere.cpp
@@ -0,0 +1,127 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+void toLower(string &command){
+    transform(command.begin(), command.end(), command.begin(), [](unsigned char c){
+        return tolower(c);
+        
+    });
+}
+void toUpper(string &command){
+    transform(command.begin(), command.end(), command.begin(), [](unsigned char c){
+        return toupper(c);
+        
+    });
+}
+
+
+/*
+
+for(int i =0; i < 26;i ++){
+        for(int j = 0; j < 26; j ++){
+            
+            cout << Vingere[i][j] << " ";
+            
+        }
+        cout << endl;
+    }
+
+
+*/
+
+
+int main(int argc, char* argv[])
+{   
+    char Vingere[26][26];
+    
+    for(int i =0; i < 26;i ++){
+        for(int j = 0; j < 26; j ++){
+            
+            Vingere[i][j]= 'a' +(i+j)%26;
+            
+        }
+    }
+    
+    
+    bool passSet = false, exitbool = false;;
+    string passKey = "";
+    string line, command, arguement;
+    
+    while(1){
+        
+        getline(cin , line);
+        istringstream iss(line);
+        iss >> command >> arguement;
+        
+        toLower(command);
+        toLower(arguement);
+        
+        if(command == "passkey"){
+            
+            passKey = arguement;
+            passSet = true;
+            
+        }
+        else if(command == "encrypt"){
+            
+            if(!passSet){
+                cout << "ERROR Password not set"<< endl;
+                continue;
+            }
+            string ret = "";
+            int n = arguement.length();
+            for(int i =0; i < n ; i ++){
+                ret = ret + Vingere[arguement[i]-'a'][passKey[i%passKey.length()]-'a'];
+            }
+            toUpper(ret);
+            cout << "RESULT "<< ret << endl;          
+        }
+        else if(command == "decrypt"){
+            
+            if(!passSet){
+                cout << "ERROR Password not set"<< endl;
+            }
+            string ret = "";
+            int n = arguement.length();
+            for(int i =0; i < n ; i ++){
+                int hold =(arguement[i]- passKey[i%passKey.length()] );
+                if(hold < 0)
+                    hold += 26;
+                ret = ret + (char)('a'+hold);
+            }
+            toUpper(ret);
+            cout << "RESULT "<< ret << endl; 
+            
+        }
+        else if(command == "exit"){
+            
+            exitbool = true;
+            
+        }else{
+            continue;
+        }
+        
+        if(exitbool)
+        break;
+        
+        command = "";
+        arguement = "";
+        
+    }
+    
+    
+    cout << "command: "<< command << "arguement: "<< arguement;
+
+    return 0;
+}
